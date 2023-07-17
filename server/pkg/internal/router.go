c27062c55d6cd2c70c3f3b3950da1fc3b730b3a1
Andrew Wei <a.nowei42@gmail.com>
2023-07-17 03:13:02 -0700

    init server boilerplate

---
+++ server/pkg/internal/router.go
@@ -0,0 +1,62 @@
+package api
+
+import (
+	"encoding/json"
+	"net/http"
+)
+
+//go:generate go run github.com/deepmap/oapi-codegen/cmd/oapi-codegen --config=config.yaml ../../../api/openapi.yaml
+
+type Server struct{}
+
+func NewServer() ServerInterface {
+	return &Server{}
+}
+func (s *Server) RegisterDonation(w http.ResponseWriter, r *http.Request) {
+	var donation DonationRegistration
+	if err := json.NewDecoder(r.Body).Decode(&donation); err != nil {
+		w.WriteHeader(400)
+		w.Write([]byte("error decoding the data"))
+		return
+	}
+	// Write information to db
+
+	// return success
+}
+
+func (s *Server) DistributeDonation(w http.ResponseWriter, r *http.Request) {
+	var data DonationDistribution
+	if err := json.NewDecoder(r.Body).Decode(&data); err != nil {
+		w.WriteHeader(400)
+		w.Write([]byte("error decoding the data"))
+		return
+	}
+	// Get donation
+
+	// Aggregate all distributions of this donation
+
+	// If new distribution + aggregate < original donation, add a new distribution
+
+	// return success
+}
+
+func (s *Server) GetDonationInventoryReport(w http.ResponseWriter, r *http.Request) {
+
+	// For each type
+
+	// For all donations for the type
+	// For all distributions for the donation
+
+	// return result
+}
+
+func (s *Server) GetDonorReport(w http.ResponseWriter, r *http.Request) {
+
+	// For each donor
+
+	// Aggregate donations by type
+
+	// Aggregate distributions by type
+
+	// return result
+}
