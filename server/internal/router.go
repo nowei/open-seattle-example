01c29a4faa77fceae66f2bb2cae8a0fc702f25d7
Andrew Wei <a.nowei42@gmail.com>
2023-07-22 23:40:30 -0700

    init response code

--- server/internal/router.go
+++ server/internal/router.go
@@ -2,13 +2,22 @@ package api
 
 import (
 	"encoding/json"
-	"log"
 	"net/http"
+
+	"github.com/nowei/open-seattle-example/server/internal/logger"
 )
 
 //go:generate go run github.com/deepmap/oapi-codegen/cmd/oapi-codegen --config=config.yaml ../../../api/openapi.yaml
 
 type Server struct{}
+
+var log = logger.GetLogger().Sugar()
+
+func respond(w http.ResponseWriter, statusCode int, obj any) {
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(statusCode)
+	json.NewEncoder(w).Encode(obj)
+}
 
 func NewServer() ServerInterface {
 	return &Server{}
@@ -23,10 +32,11 @@ func (s *Server) RegisterDonation(w http.ResponseWriter, r *http.Request) {
 	// Write information to db
 
 	// return success
+	// respond(w, http.StatusCreated, obj)
 }
 
 func (s *Server) DistributeDonation(w http.ResponseWriter, r *http.Request) {
-	log.Printf("Stuffs")
+	log.Infof("Stuffs")
 	var data DonationDistribution
 	if err := json.NewDecoder(r.Body).Decode(&data); err != nil {
 		w.WriteHeader(400)
@@ -40,6 +50,7 @@ func (s *Server) DistributeDonation(w http.ResponseWriter, r *http.Request) {
 	// If new distribution + aggregate < original donation, add a new distribution
 
 	// return success
+	// respond(w, http.StatusCreated, obj)
 }
 
 func (s *Server) GetDonationInventoryReport(w http.ResponseWriter, r *http.Request) {
@@ -50,6 +61,7 @@ func (s *Server) GetDonationInventoryReport(w http.ResponseWriter, r *http.Reque
 	// For all distributions for the donation
 
 	// return result
+	// respond(w, http.StatusOK, obj)
 }
 
 func (s *Server) GetDonorReport(w http.ResponseWriter, r *http.Request) {
@@ -61,4 +73,5 @@ func (s *Server) GetDonorReport(w http.ResponseWriter, r *http.Request) {
 	// Aggregate distributions by type
 
 	// return result
+	// respond(w, http.StatusOK, obj)
 }
